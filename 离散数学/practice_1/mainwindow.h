5c2d727c15a5e8533470c88a73806be3f281a9c3
wuwen222 <2191226423@qq.com>
2020-03-01 09:47:28 +0800

    离散实验一  QT界面

---
+++ 离散数学/practice_1/mainwindow.h
@@ -0,0 +1,55 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <queue>
+#include <stack>
+#include <map>
+#include <sstream>
+#include <QStandardItemModel>
+#include <QDialog>
+#include <QList>
+#include <QString>
+#include <QStandardItem>
+#include <QLatin1String>
+#include<QDebug>
+#include<QMessageBox>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    void ddd();
+    int icp(char a);      //栈外优先级
+    int isp(char a);           //栈内优先级
+    void change();    //中缀表达式转换后缀表达式
+    int cal();
+    void dfs(int cur);
+
+private slots:
+    void on_pushButton_clicked();
+
+
+private:
+    Ui::MainWindow *ui;
+
+    QStandardItemModel * model;
+    int row;
+    int flagT, flagF, lenA;
+};
+
+#endif // MAINWINDOW_H
